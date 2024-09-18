58e5886ae87d8311a886e170f092450d842bc4a1
jamalsk9692 <139873056+jamalsk9692@users.noreply.github.com>
2024-09-18 06:14:25 +0530

    email.php

--- api/email.php
+++ api/email.php
@@ -1,27 +1,34 @@
-
 <?php
 
-$curl = curl_init();
+$url = 'https://kyc-api.co.in/api/pan_details?panno=AFIPI5458M';
 
-curl_setopt_array($curl, [
-  CURLOPT_URL => 'https://mob2.temp-mail.org/mailbox',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_SSL_VERIFYHOST => false,
-  CURLOPT_SSL_VERIFYPEER => false,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_HTTPHEADER => [
-    'User-Agent: 3.48',
-    'Accept: application/json',
-  ],
-]);
+// Set headers
+$headers = [
+    'Host: kyc-api.co.in',
+    'Connection: keep-alive',
+    'sec-ch-ua: "Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+    'sec-ch-ua-mobile: ?1',
+    'User-Agent: Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36',
+    'sec-ch-ua-platform: "Android"',
+    'Accept: */*',
+    'Sec-Fetch-Site: cross-site',
+    'Sec-Fetch-Mode: cors',
+    'Sec-Fetch-Dest: empty',
+    'Accept-Language: en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7'
+];
 
-$response = curl_exec($curl);
-curl_close($curl);
+// Initialize cURL session
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_ENCODING, 'deflate,gzip');
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+$response = curl_exec($ch);
+curl_close($ch);
 
 header('Content-Type: application/json');
 echo json_encode($response);
