5d4cf62e716b20364e2abfac095984dc228448bd
jamalsk9692 <139873056+jamalsk9692@users.noreply.github.com>
2024-09-18 06:43:54 +0530

    email.php

--- api/email.php
+++ api/email.php
@@ -1,37 +1,3 @@
 <?php
 phpinfo();
-
-$url = 'https://kyc-api.co.in/api/pan_details?panno=AFIPI5458M';
-
-// Set headers
-$headers = [
-    'Host: kyc-api.co.in',
-    'Connection: keep-alive',
-    'sec-ch-ua: "Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
-    'sec-ch-ua-mobile: ?1',
-    'User-Agent: Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36',
-    'sec-ch-ua-platform: "Android"',
-    'Accept: */*',
-    'Sec-Fetch-Site: cross-site',
-    'Sec-Fetch-Mode: cors',
-    'Sec-Fetch-Dest: empty',
-    'Accept-Language: en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7'
-];
-
-// Initialize cURL session
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_ENCODING, 'deflate,gzip');
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
-$response = curl_exec($ch);
-curl_close($ch);
-
-header('Content-Type: application/json');
-echo json_encode($response);
-
 ?>
